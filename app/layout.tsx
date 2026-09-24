67aebb73165f06a9a3643e7271a1f8a30ef2e5ab
surinder2003k <surinder2003k@gmail.com>
2026-09-24 23:03:10 +0530

    perf: reduce redundant analytics initialization

--- app/layout.tsx
+++ app/layout.tsx
@@ -160,20 +160,14 @@ export default async function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="preconnect" href="https://www.googletagmanager.com" />
+        <link rel="preconnect" href="https://www.google-analytics.com" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
       </head>
       <body className={`${sora.variable} ${jetbrainsMono.variable} antialiased selection:bg-cyan-500/30 selection:text-white`}>
-        <Script id="gtm-script" strategy="lazyOnload">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-N3CBBBLM');`}
-        </Script>
-
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-HSZJRM7GKQ"
           strategy="lazyOnload"
